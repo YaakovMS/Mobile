c7c2b21a879d1a08cf364c8a19a3efbc851e81f0
Yaakov Meir <ymeir02@gmail.com>
2023-10-04 15:44:21 -0300

    Close #68 : estilizando tela de cadastro de atividades

--- Task/src/screens/NovaAtividade.js
+++ Task/src/screens/NovaAtividade.js
@@ -1,33 +1,34 @@
-import React, { useEffect , useState} from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput } from 'react-native'; 
+import { Picker } from '@react-native-picker/picker';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { useAtividades } from '../context/AtividadesContext';
 import { Button } from 'react-native-elements';
 import CustomInput from '../components/CostumeInput';
 import { verificarProjetoSelecionado } from '../context/ProjetoContext';
 
+import NovaAtividadeStyle from '../styles/NovaAtividadeStyle';
+
 const NovaAtividade = () => {
   const route = useRoute();
   const navigation = useNavigation();
   const { useSalvarAtividade } = useAtividades();
   const salvarAtividade = useSalvarAtividade();
-  const projetoId = route.params?.projetoId; // Usando ? para verificar se projetoId está definido
+  const projetoId = route.params?.projetoId;
 
   useEffect(() => {
     if (!projetoId) {
-      // Se projetoId não estiver definido, redirecione para 'Projetos'
       navigation.navigate('Projetos');
     }
   }, [projetoId, navigation]);
 
   const [novaAtividade, setNovaAtividade] = useState('');
   const [dataLimite, setDataLimite] = useState('');
-  const [prioridade, setPrioridade] = useState('Baixa');
   const [descricao, setDescricao] = useState('');
+  const [prioridade, setPrioridade] = useState('Baixa');
 
   const salvarNovaAtividade = () => {
     if (!projetoId) {
-      // Se projetoId não estiver definido, redirecione para 'Projetos'
       navigation.navigate('Projetos');
       return;
     }
@@ -48,39 +49,54 @@ const NovaAtividade = () => {
     salvarAtividade(novaAtividadeObj);
     setNovaAtividade('');
     setDataLimite('');
-    setPrioridade('Baixa');
     setDescricao('');
+    setPrioridade('Baixa');
   };
 
   return (
-    <View>
-      <Text>Projeto selecionado: {projetoId || 'Nenhum projeto selecionado'}</Text>
+    <View style={NovaAtividadeStyle.container}>
+      <Text style={NovaAtividadeStyle.title}>
+        Projeto selecionado: {projetoId || 'Nenhum projeto selecionado'}
+      </Text>
       <Text>Criar Nova Atividade:</Text>
       <CustomInput
+        style={NovaAtividadeStyle.input}
         placeholder="Nova Atividade"
         value={novaAtividade}
         onChangeText={(text) => setNovaAtividade(text)}
       />
       <CustomInput
+        style={NovaAtividadeStyle.input}
         placeholder="Data Limite"
         value={dataLimite}
         onChangeText={(text) => setDataLimite(text)}
       />
       <CustomInput
-        placeholder="Prioridade"
-        value={prioridade}
-        onChangeText={(text) => setPrioridade(text)}
-      />
-      <CustomInput
+        style={[NovaAtividadeStyle.input, NovaAtividadeStyle.largeInput]}
         placeholder="Descrição"
         value={descricao}
         onChangeText={(text) => setDescricao(text)}
+        multiline={true}
+        numberOfLines={4}
       />
+      {/* Utilize o Picker padrão do React Native para a seleção de prioridade */}
+      <Picker
+        style={NovaAtividadeStyle.picker}
+        selectedValue={prioridade}
+        onValueChange={(itemValue) => setPrioridade(itemValue)}
+      >
+        <Picker.Item label="Baixa Prioridade" value="baixa" />
+        <Picker.Item label="Média Prioridade" value="media" />
+        <Picker.Item label="Alta Prioridade" value="alta" />
+      </Picker>
       <Button
+        buttonStyle={NovaAtividadeStyle.button}
         onPress={salvarNovaAtividade}
         title="Salvar Atividade"
       />
       <Button
+        buttonStyle={[NovaAtividadeStyle.button, NovaAtividadeStyle.backButton]}
+        titleStyle={NovaAtividadeStyle.backButtonText}
         onPress={() => navigation.goBack()}
         title="Voltar"
       />
---
+++ Task/src/styles/NovaAtividadeStyle.js
@@ -0,0 +1,48 @@
+import { StyleSheet } from 'react-native';
+
+
+const NovaAtividadeStyle = StyleSheet.create({
+  container: {
+    flex: 1, // Garante que o contêiner ocupe todo o espaço disponível
+    justifyContent: 'center', // Centraliza verticalmente os componentes
+    alignItems: 'center', // Centraliza horizontalmente os componentes
+    padding: 16, // Adiciona um espaçamento interno
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 16,
+  },
+  input: {
+    width: '100%', // Faz com que o campo de entrada ocupe a largura completa
+    marginBottom: 12,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    padding: 8,
+  },
+  picker: {
+    width: 300,
+    marginBottom: 10,
+    color: '#4B5F83',
+    alignContent: 'center'
+  },
+  pickerLabel: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: '#4B5F83',
+  },
+  button: {
+    backgroundColor: '#4B5F83',
+    marginTop: 8,
+  },
+  backButton: {
+    backgroundColor: '#D9D9D9',
+    marginTop: 8,
+  },
+  backButtonText: {
+    color: '#000',
+  },
+});
+
+export default NovaAtividadeStyle;
