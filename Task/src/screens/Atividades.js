bfe042ea1212a12378f2944ea65f6a7480cc9231
Yaakov Meir <ymeir02@gmail.com>
2023-10-04 17:37:13 -0300

    Close #67 : estilizando tela de Atividades

--- Task/src/screens/Atividades.js
+++ Task/src/screens/Atividades.js
@@ -1,26 +1,30 @@
-import React, { useEffect } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { useRoute, useNavigation } from '@react-navigation/native';
-import { useAtividades } from '../context/AtividadesContext';
-import { Button } from 'react-native-elements';
-import { verificarProjetoSelecionado } from '../context/ProjetoContext';
+import React, { useEffect } from "react";
+import { View, Text, FlatList,TouchableOpacity } from "react-native";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { useAtividades } from "../context/AtividadesContext";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Button } from "react-native-elements";
+import { verificarProjetoSelecionado } from "../context/ProjetoContext";
+import AtividadeStyle from "../styles/AtividadeStyle";
 
 const Atividades = () => {
   const route = useRoute();
   const navigation = useNavigation();
   const { atividades } = useAtividades();
-  const projetoId = route.params?.projetoId; // Usando ? para verificar se projetoId está definido
+  const projetoId = route.params?.projetoId;
 
   useEffect(() => {
     if (!projetoId) {
-      // Se projetoId não estiver definido, redirecione para 'Projetos'
-      navigation.navigate('Projetos');
+      navigation.navigate("Projetos");
       return;
     }
 
-    const projetoSelecionado = verificarProjetoSelecionado(projetoId, navigation);
+    const projetoSelecionado = verificarProjetoSelecionado(
+      projetoId,
+      navigation
+    );
     if (!projetoSelecionado) {
-      navigation.navigate('Projetos'); // Redireciona para 'Projetos' se o projeto não estiver selecionado
+      navigation.navigate("Projetos");
     }
   }, [projetoId, navigation]);
 
@@ -29,28 +33,38 @@ const Atividades = () => {
   );
 
   return (
-    <View>
-      <Text>Projeto selecionado: {projetoId || 'Nenhum projeto selecionado'}</Text>
-      <Text>Atividades do Projeto:</Text>
+    <View style={AtividadeStyle.screenAtividades}>
+      <Text style={AtividadeStyle.screenAtividadesTitle}>
+        Atividades do Projeto:
+      </Text>
+      <Text style={AtividadeStyle.screenAtividadesProjetoId}>
+        Projeto selecionado: {projetoId || "Nenhum projeto selecionado"}
+      </Text>
+
       <FlatList
         data={atividadesDoProjeto}
         keyExtractor={(atividade) => atividade.id}
         renderItem={({ item }) => (
-          <View>
-            <Text>Nome: {item.nome}</Text>
+          <View style={AtividadeStyle.atividadeContainer}>
+            <Text style={AtividadeStyle.nome}>Nome: {item.nome}</Text>
             <Text>Data Limite: {item.dataLimite}</Text>
-            <Text>Prioridade: {item.prioridade}</Text>
-            <Text>Descrição: {item.descricao}</Text>
+            <Text style={AtividadeStyle.prioridade}>
+              Prioridade: {item.prioridade}
+            </Text>
+            <Text style={AtividadeStyle.descricao}>
+              Descrição: {item.descricao}
+            </Text>
           </View>
         )}
       />
-      <Button
-        onPress={() => navigation.navigate('Nova Atividade', { projetoId: projetoId })}
-        title="Nova Atividade"
-      />
+     <TouchableOpacity
+  onPress={() => navigation.navigate('Nova Atividade', { projetoId: projetoId })}
+  style={AtividadeStyle.floatingButton} // Use o novo estilo aqui
+>
+<Icon name="plus" size={32} color="#fff" />
+</TouchableOpacity>
     </View>
   );
 };
 
 export default Atividades;
-
---
+++ Task/src/styles/AtividadeStyle.js
@@ -0,0 +1,60 @@
+import { StyleSheet } from "react-native";
+
+const AtividadeStyle = StyleSheet.create({
+  screenAtividades: {
+    flex: 1,
+    backgroundColor: "#F5F5F5", // Define a cor de fundo
+    paddingHorizontal: 16, // Espaçamento horizontal interno
+    paddingTop: 16, // Espaçamento superior interno
+  },
+  screenAtividadesProjetoId: {
+    fontSize: 18,
+    color: "#4B5F83",
+    marginBottom: 25,
+  },
+
+  screenAtividadesTitle: {
+    color: "#4B5F83",
+    fontSize: 25,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  atividadeContainer: {
+    backgroundColor: "lightgray", // Define a cor de fundo das atividades
+    marginBottom: 16, // Espaçamento inferior entre atividades
+    padding: 16, // Espaçamento interno das atividades
+    borderRadius: 8, // Define um raio para os cantos das atividades
+  },
+  nome: {
+    fontWeight: "bold",
+    fontSize: 18, // Define o tamanho da fonte para o nome
+  },
+  dataLimite: {
+    color: "gray",
+  },
+  prioridade: {
+    color: "red",
+  },
+  descricao: {
+    marginTop: 5,
+  },
+  ButtonNovaAtividade: {
+    marginBottom: 30,
+  },
+ 
+  floatingButton: {
+    position: 'absolute',
+    right: 20,
+    bottom: 20,
+    backgroundColor: '#4B5F83', // Cor do botão
+    borderRadius: 32,
+    width: 64,
+    height: 64,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 4,
+  },
+});
+
+export default AtividadeStyle;
